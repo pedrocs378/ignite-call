bea2218eb8b3edfee81be4fc3d34eac768040696
Pedro <pedrocs378@gmail.com>
2023-01-02 20:17:30 -0300

    feat: add api route to create scheduling

---
+++ src/pages/api/users/[username]/schedule.api.ts
@@ -0,0 +1,70 @@
+import dayjs from 'dayjs'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+import { prisma } from '../../../../lib/prisma'
+
+const createSchedulingBody = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  observations: z.string().nullable(),
+  date: z.string().datetime(),
+})
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const username = String(req.query.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return res.status(400).json({ message: 'Usuário não encontrado' })
+  }
+
+  const { name, email, observations, date } = createSchedulingBody.parse(
+    req.body,
+  )
+
+  const schedulingDate = dayjs(date).startOf('hour')
+
+  if (schedulingDate.isBefore(new Date())) {
+    return res.status(400).json({
+      message: 'Data de agendamento já passou',
+    })
+  }
+
+  const conflictingScheduling = await prisma.scheduling.findFirst({
+    where: {
+      user_id: user.id,
+      date: schedulingDate.toDate(),
+    },
+  })
+
+  if (conflictingScheduling) {
+    return res.status(400).json({
+      message: 'Já existe um agendamento agendado no mesmo horário',
+    })
+  }
+
+  await prisma.scheduling.create({
+    data: {
+      name,
+      email,
+      observations,
+      date: schedulingDate.toDate(),
+      user_id: user.id,
+    },
+  })
+
+  return res.status(201).end()
+}
